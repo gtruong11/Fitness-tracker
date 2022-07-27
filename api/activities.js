280b4e7248562db2f64c5fcabf56d2d2ec87f22f
Garrett Truong <aznkungfubunnee@gmail.com>
2022-07-27 13:45:10 -0500

    finished getallposts

--- api/activities.js
+++ api/activities.js
@@ -1,12 +1,26 @@
 const express = require('express');
-const router = express.Router();
+const activitiesRouter = express.Router();
+const {getAllActivities, getActivityById, createActivity, updateActivity, getActivityByName}= require("../db")
+const {requireUser} = require("./utils")
 
-// GET /api/activities/:activityId/routines
+
 
 // GET /api/activities
+activitiesRouter.get('/', async (req,res,next)=>{
+    console.log("outside try")
+    try{
+        console.log("inside try")
+        const activities = await getAllActivities();
+        console.log(activities, "Show me the money 14")
+        res.send(activities)
+    }catch (error){
+        next(error)
+    }
+})
+// GET /api/activities/:activityId/routines
 
 // POST /api/activities
 
 // PATCH /api/activities/:activityId
 
-module.exports = router;
+module.exports = activitiesRouter;
