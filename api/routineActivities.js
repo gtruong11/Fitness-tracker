d6d5f364a06fcdf141d8424fe93f741247e77091
Garrett Truong <aznkungfubunnee@gmail.com>
2022-07-28 13:27:12 -0500

    almost done

--- api/routineActivities.js
+++ api/routineActivities.js
@@ -1,31 +1,25 @@
 const express = require('express');
 const routineActivitiesRouter = express.Router();
 const { requireUser } = require('./utils')
-const { getRoutineActivityById, updateRoutineActivity } = require('../db')
+const { getRoutineActivityById, updateRoutineActivity, destroyRoutineActivity, canEditRoutineActivity } = require('../db')
 
 // PATCH /api/routine_activities/:routineActivityId
 routineActivitiesRouter.patch('/:routineActivityId', requireUser, async (req, res, next) => {
-    console.log("inside of patch")
     const { routineActivityId } = req.params;
     console.log(routineActivityId, "first check")
     const { count, duration } = req.body;
     console.log( count, duration, "second check")
     const { username } = req.user
-    console.log("third check")
     const updatedRoutineActivity = await getRoutineActivityById(routineActivityId)
-    console.log("outside of try 12")
     try {
-        console.log("inside of try 14")
         if(updatedRoutineActivity.id !== req.user.id) {
-            console.log("inside of if 16")
             res.status(403)
             next ({
                 name: "User is not found",
                 message: `User ${username} is not allowed to update In the evening`
             })
         } else {
-            console.log("inside of else")
-            const upToDateRoutineActivity = await updateRoutineActivity({ id: count, duration });
+            const upToDateRoutineActivity = await updateRoutineActivity({ id: routineActivityId, count, duration });
 
             res.send(upToDateRoutineActivity)
         }
@@ -34,5 +28,22 @@ routineActivitiesRouter.patch('/:routineActivityId', requireUser, async (req, re
     }
 })
 // DELETE /api/routine_activities/:routineActivityId
+routineActivitiesRouter.delete('/:routineActivityId', requireUser, async (req,res,next)=>{
+    const { username}= req.user
+    try{
+        if(!await canEditRoutineActivity(req.params.routineActivityId, req.user.id)) {
+            res.status(403)
+            next ({
+                name: "User is not found",
+                message: `User ${username} is not allowed to delete In the afternoon`
+            })
+        } else {
+            const deleteActivity = await destroyRoutineActivity(req.params.routineActivityId)
+
+            res.send(deleteActivity)
+        }
+    } catch (error) {
+        next (error)
+    }})
 
 module.exports = routineActivitiesRouter;
